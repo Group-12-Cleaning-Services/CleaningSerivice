47cff3fe858b561c5fad57139f2962f0ec2a9ec2
lord <konadulordkweku@gmai.com>
2024-01-11 00:07:42 +0000

    modify setting

--- CleaningSerivice/settings.py
+++ CleaningSerivice/settings.py
@@ -16,7 +16,7 @@ load_dotenv()  # take environment variables from .env.
 from pathlib import Path
 import os
 from datetime import datetime, timedelta
-import django_heroku
+import dj_database_url
 
 
 
@@ -87,11 +87,20 @@ WSGI_APPLICATION = "CleaningSerivice.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
+    ),
 }
 
 
