3583927f4db77d15660036db955caffae91698ea
lord <konadulordkweku@gmai.com>
2024-01-13 12:50:12 +0000

    fix media url error

--- core/urls.py
+++ core/urls.py
@@ -46,5 +46,3 @@ urlpatterns = [
     path('notification/all/', NotificationViewset.as_view({'get': 'list'})),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
